308287abb399a2fae6995c3f8237ab184e776f32
Philipp Ucke <64527676+pucke-uninow@users.noreply.github.com>
2022-11-10 00:12:06 +0100

    Create aliases.zsh

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,10 @@
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+
+# git
+alias gl="git pull"
+alias gp="git push"
+alias gcm="git checkout $(git_main_branch)"
+alias gcd="git checkout $(git_develop_branch)"
+
