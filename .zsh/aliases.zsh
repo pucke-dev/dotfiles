935bbab9c5686c9976d5915b145e569ebd74f675
Philipp Ucke <64527676+pucke-uninow@users.noreply.github.com>
2022-11-10 00:17:44 +0100

    add git util functions

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -3,6 +3,33 @@ alias h="helm"
 alias tf="terraform"
 
 # git
+
+# Check if main exists and use instead of master
+function git_main_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  local ref
+  for ref in refs/{heads,remotes/{origin,upstream}}/{main,trunk}; do
+    if command git show-ref -q --verify $ref; then
+      echo ${ref:t}
+      return
+    fi
+  done
+  echo master
+}
+
+# Check for develop and similarly named branches
+function git_develop_branch() {
+  command git rev-parse --git-dir &>/dev/null || return
+  local branch
+  for branch in dev devel development; do
+    if command git show-ref -q --verify refs/heads/$branch; then
+      echo $branch
+      return
+    fi
+  done
+  echo develop
+}
+
 alias gl="git pull"
 alias gp="git push"
 alias gcm="git checkout $(git_main_branch)"
