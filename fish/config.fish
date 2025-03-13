4f2b78c64de9f57f74112f3bf8c8e3b43a0cd595
Philipp Ucke <philipp.ucke@lovoo.com>
2025-03-13 21:40:27 +0100

    add aliases, envs, rust bin to path

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,18 @@ if status is-interactive
     atuin init fish | source
 end
 
-export XDG_CONFIG_HOME="$HOME/.config"
 
+### Environment Variables ###
+export XDG_CONFIG_HOME="$HOME/.config"
+export EDITOR="nvim"
+export STARSHIP_CONFIG="~/.config/starship/starship.toml"
+
+# Podman Configuration
+export TESTCONTAINERS_RYUK_DISABLED=true
+export DOCKER_HOST=unix://$(podman machine inspect --format '{{.ConnectionInfo.PodmanSocket.Path}}')
+
+
+### Aliases ###
 alias ls="eza -1 -l -F -a"
 alias cat="bat --paging=never"
 alias k="kubectl"
@@ -13,17 +23,23 @@ alias v="nvim"
 alias gs="git status"
 alias gp="git push"
 alias gl="git pull"
+alias gcm="git commit -m"
+alias gca="git commit --amend"
 alias gcd="git checkout develop"
 
 alias kfp="k get pods --all-namespaces | fzf"
 
-export STARSHIP_CONFIG="~/.config/starship/starship.toml"
+alias docker="podman"
+
+
+### Setup Tools ###
 starship init fish | source
 
 zoxide init fish --cmd cd | source
 
 fish_add_path -g $(which go)
 fish_add_path -g "$HOME/.go/bin"
+fish_add_path -g "$HOME/.cargo/bin"
 
 set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
 mkdir -p ~/.config/fish/completions
